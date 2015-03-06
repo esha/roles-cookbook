aaee3ea7d7fe4934848d712c705bd87d1962df98
Christopher Elkins <chrise@esha.com>
2015-03-06 09:12:39 -0800

    Set default attributes for our Chef server

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: roles
+# Attribute:: default
+#
+
+default['chef_client']['server_url'] = 'https://api.opscode.com/organizations/esha'
+default['chef_client']['validation_client_name'] = 'esha-validator'
